bd19b7dc5f80081faeafe0bf58977e740c1548c6
Raffael47 <132432134+Raffael47@users.noreply.github.com>
2023-07-25 21:37:46 +0700

    token middleware

---
+++ projects/server/src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    verifyToken: async(req, res, next) => {
+        try {
+            const token = req.headers.Authorization;
+            if (!token) throw {
+                status: false,
+                message: 'Unauthorized Request'
+            };
+            token = token.split(' ')[1];
+
+            let verifiedAccount = jwt.verify(token, process.env.KEY_JWT);
+            req.token = token;
+            req.account = verifiedAccount;
+            next();
+
+        } catch (err) {
+            res.status(401).send(err);
+        }
+    },
+    verifyAdmin: async(req, res, next) => {
+        if (!req.user.isAdmin) res.status(401).send({
+            status: false,
+            message: 'Access Denied'
+        });
+        next();
+    }
+};
