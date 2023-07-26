806519f7dacf18c6b880ba59e30e2a4515d39a85
Raffael47 <132432134+Raffael47@users.noreply.github.com>
2023-07-26 15:33:18 +0700

    report (#6)
    * report
    
    * report 2

--- projects/server/src/routers/reportRouter.js
+++ projects/server/src/routers/reportRouter.js
@@ -1,10 +1,11 @@
 const { reportController } = require('../controllers');
-const { verifyToken, verifyCashier } = require('../middleware/auth');
+const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
 const router = require('express').Router();
 
-router.get('/', verifyToken, verifyCashier, reportController.getReport);
-router.get('/sales', verifyToken, verifyCashier, reportController.getSalesPer);
-router.get('/products', verifyToken, verifyCashier, reportController.getProductsSold);
+router.get('/', verifyToken, verifyAdmin, reportController.getReport);
+router.get('/sales', verifyToken, verifyAdmin, reportController.getSalesPer);
+router.get('/products', verifyToken, verifyAdmin, reportController.getProductsSold);
+
 
 module.exports = router;
