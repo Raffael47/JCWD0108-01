1328cd9331fc42e58bdd02dbbf0e4b0f16d6f2c3
Raffael47 <132432134+Raffael47@users.noreply.github.com>
2023-07-28 10:13:13 +0700

    Homepage Template (#23)
    * Database design (#1)
    
    * token middleware (#2)
    
    * Auth valid (#3)
    
    * token middleware
    
    * auth validator
    
    * Transaction (#4)
    
    * token middleware
    
    * auth validator
    
    * transaction
    
    * report (#5)
    
    * report (#6)
    
    * report
    
    * report 2
    
    * redux setup (#7)
    
    * components update 1 (#8)
    
    * report update 2
    
    * homepage template (#9)
    
    ---------
    
    Co-authored-by: Fathir achmad sabiilah <101498067+Fathir-achmad@users.noreply.github.com>

---
+++ projects/client/src/pages/homePage.jsx
@@ -0,0 +1,17 @@
+import { Grid, GridItem } from '@chakra-ui/react'
+
+export const HomePage = () => {
+    return (
+    <Grid templateColumns='2fr 8fr 3fr' bgColor='' w='100vw' h='100vh' overflowX={'hidden'}>
+    <GridItem colSpan={1} w='100%' h={'100%'} bg='red.500'>
+        {/* navbar */}
+    </GridItem>
+    <GridItem colSpan={1} w='100%' h={'100%'} bg='gray.500'>
+        {/* Product & Category */}
+    </GridItem>
+    <GridItem colSpan={1} w='100%' h={'100%'} bg='blue.500' p={2}>
+      {/* <Receipt/> */}
+    </GridItem>
+    </Grid>
+    )
+}
