4ed56ac98c65c0d9040d2375c29d044337a04a06
Fathir achmad sabiilah <101498067+Fathir-achmad@users.noreply.github.com>
2023-08-03 18:50:08 +0700

    import receipt di homepage (#47)

--- projects/client/src/pages/homePage.jsx
+++ projects/client/src/pages/homePage.jsx
@@ -1,17 +1,31 @@
-import { Grid, GridItem } from '@chakra-ui/react'
-import { ProductCategory } from '../components/productCategory'
-import { Search } from '../components/product/searchProduct'
-import { Navbar } from '../components/navBar'
+import { Grid, GridItem } from "@chakra-ui/react";
+import { ProductCategory } from "../components/productCategory";
+import { Search } from "../components/product/searchProduct";
+import { Navbar } from "../components/navBar";
+import { Receipt } from "../components/transaction/receipt";
 
 export const HomePage = () => {
-    return (
-    <Grid templateColumns='repeat(11, 1fr)' bgColor='' w='100%' h='100vh' overflowX={'hidden'}>
-    <GridItem colSpan={{base: 11, md: 8}} w='100%' h={'100%'} bg='#2d2d2d'>
-        <ProductCategory/>
-    </GridItem>
-    <GridItem display={{base: 'none', md: 'block'}} colSpan={{base: 0, md: 3}} w='100%' h={'100%'} bg='#2d2d2d' p={2}>
-        <Receipt
-    </GridItem>
+  return (
+    <Grid
+      templateColumns="repeat(11, 1fr)"
+      bgColor=""
+      w="100%"
+      h="100vh"
+      overflowX={"hidden"}
+    >
+      <GridItem colSpan={{ base: 11, md: 8 }} w="100%" h={"100%"} bg="#2d2d2d">
+        <ProductCategory />
+      </GridItem>
+      <GridItem
+        display={{ base: "none", md: "block" }}
+        colSpan={{ base: 0, md: 3 }}
+        w="100%"
+        h={"100%"}
+        bg="#2d2d2d"
+        p={2}
+      >
+        <Receipt />
+      </GridItem>
     </Grid>
-    )
-}
+  );
+};
